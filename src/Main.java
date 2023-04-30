ea0b2fb1bc1f30c297ed9893cdb417fc77b79311
Pablo Zhurbickiy <pavelkortp@gmail.com>
2023-04-30 03:29:48 +0300

    parse from string to int complete

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int a = NumberFormatter.parseInt("1234");
         System.out.println(a);
-
-        String b = String.valueOf(a);
+        a = NumberFormatter.parseInt("-1234");
+        System.out.println(a);
+        a = NumberFormatter.parseInt("+1234");
+        System.out.println(a);
     }
 }
